d43c7223b4310b09839aa96ca5c7300894f2d527
Aadesh Devganiya <162079120+aadeshdevganiya@users.noreply.github.com>
2025-04-14 15:46:35 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
+const btn = document.getElementById('buttonn');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'service_bo5gc0f';
+   const templateID = 'template_bxt1t6l';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
+
+
+
 
 new WOW().init();
 
@@ -125,26 +148,7 @@ var preogressbar = document.getElementById("preogressbar")
         }
      
 
-const btn = document.getElementById('buttonn');
 
-document.getElementById('form')
- .addEventListener('submit', function(event) {
-   event.preventDefault();
-
-   btn.value = 'Sending...';
-
-   const serviceID = 'service_bo5gc0f';
-   const templateID = 'template_bxt1t6l';
-
-   emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
-      btn.value = 'Send Email';
-      alert('Sent!');
-    }, (err) => {
-      btn.value = 'Send Email';
-      alert(JSON.stringify(err));
-    });
-});
 
 
 
