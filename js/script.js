1bbeea38716668c4123ee5036fdbaa832e696200
Aadesh Devganiya <162079120+aadeshdevganiya@users.noreply.github.com>
2025-05-12 13:28:09 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -100,7 +100,7 @@ $(document).ready(function(){
 
   // typing text animation script
   var typed = new Typed(".typing", {
-      strings: ["Front end developer","Fresher", "Designer", "Freelancer"],
+      strings: ["Web Designer","Front end developer", "Designer", "Freelancer" ,"Shopify Developer"],
       typeSpeed: 100,
       backSpeed: 60,
       loop: true
@@ -113,7 +113,7 @@ $(document).ready(function(){
   });
 
   var typed = new Typed(".typing-2", {
-      strings: ["Front end developer","Fresher" , "Designer", "Freelancer"],
+      strings: ["Web Designer","Front end developer", "Designer", "Freelancer" ,"Shopify Developer"],
       typeSpeed: 100,
       backSpeed: 60,
       loop: true
